0f2601d61c991a4380f4cf81de319e5eea899c2d
Yassine ECH-CHAIRI <echchairi.yassine@gmail.com>
2025-03-18 08:02:08 +0100

    adding some animation to About page using farmer motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import aboutImg from '../assets/me.png';
 import { ABOUT_TEXT } from '../constants';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
@@ -9,16 +10,24 @@ const About = () => {
         About <span className="text-neutral-500"> Me </span>
       </h2>
       <div className="flex flex-wrap items-center">
-        <div className="w-full lg:w-1/2 lg:p-8 order-1 lg:order-1">
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5}}
+        className="w-full lg:w-1/2 lg:p-8 order-1 lg:order-1">
           <div className="flex items-center justify-center lg:justify-start">
             <img src={aboutImg} alt="About Me" className="rounded-full w-80 h-80" />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-2">
+        </motion.div>
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 0.5}}
+        className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-2">
           <div className="flex justify-center lg:justify-start">
             <p className="text-justify text-lg">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
