0bed84b5ee73c38b0d0c533ba4666b8dee18c59c
Yassine ECH-CHAIRI <echchairi.yassine@gmail.com>
2025-03-18 09:27:21 +0100

    adding some animation and  UPD on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/he.jpg";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Navbar = () => {
@@ -8,9 +8,15 @@ const Navbar = () => {
                 <img className="mx-2 w-10" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaEnvelope />
+                <a href="https://www.linkedin.com/in/yassine-ech-chairi-9444b2272/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/YassineECh-23" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="mailto:echchairi.yassine@gmail.com">
+                    <FaEnvelope />
+                </a>
             </div>
         </nav>
     );
